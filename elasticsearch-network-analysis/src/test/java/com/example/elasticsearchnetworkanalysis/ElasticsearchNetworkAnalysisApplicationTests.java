7e2485e07644508c284089649d2a3896f2d7abdb
yunachu <gooddays1202@naver.com>
2020-03-29 12:51:53 +0900

    commit

---
+++ elasticsearch-network-analysis/src/test/java/com/example/elasticsearchnetworkanalysis/ElasticsearchNetworkAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearchnetworkanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchNetworkAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
